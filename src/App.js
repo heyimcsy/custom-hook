bc9103211ce79214635e167deb7969f550e00206
최서윤 <cinnamon1995@naver.com>
2023-03-14 14:26:02 +0900

    커스텀 훅을 만들어 반복되는 문장 줄이기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import { useState } from 'react'
+import useInput from './hooks/useInput'
 
 function App() {
+  const [name, onChangeNameHandler] = useInput()
+  const [password, onChangePassHandler] = useInput()
+
+  // const [name, setName] = useState('')
+  // const [password, setPassword] = useState('')
+
+  // const onChangeNameHandler = (e) => {
+  //   setName(e.target.value)
+  // }
+  // const onChangePassHandler = (e) => {
+  //   setPassword(e.target.value)
+  // }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      CUSTOM HOOK
+      <input type="text" value={name} onChange={onChangeNameHandler} placeholder={'이름적는 곳'} />
+      <input type="password" value={password} onChange={onChangePassHandler} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
